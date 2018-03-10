f008c59048c8c86e867be068358d739006ac073b
CongTO <tcvn1985@gmail.com>
2018-03-10 09:40:08 +0700

    cap nhat script tao VM

--- scripts/create-vm.sh
+++ scripts/create-vm.sh
@@ -2,6 +2,30 @@
 
 source ~/keystonerc_admin
 
+###############################################################################
+##  Cac bien
+## IP PROVIDER Network for VMs
+PROVIDER_IP_START=192.168.20.130
+PROVIDER_IP_END=192.168.20.150
+PROVIDER_DNS=8.8.8.8
+PROVIDER_GATEWAY=192.168.20.1
+PROVIDER_SUBNET=192.168.20.0/24
+
+## IP PRIVATE Network for VMs
+PRIVATE_IP_START=172.16.85.10
+PRIVATE_IP_END=172.16.85.90
+PRIVATE_DNS=8.8.8.8
+PRIVATE_GATEWAY=172.16.85.1
+PRIVATE_SUBNET=172.16.85.0/24
+
+######
+
+# Ham dinh nghia mau cho cac thong bao in ra man hinh
+function echocolor {
+    echo "$(tput setaf 2)##### $1 #####$(tput sgr0)"
+}
+######
+
 echo "Tai image"
 sleep 3
 curl http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img | glance \
@@ -10,35 +34,71 @@ image-create --name='cirros' \
 --container-format=bare \
 --disk-format=qcow2
 
-echo "Tao external network"
+###############################################################################
+echocolor "Tao flavor"
 sleep 3
-neutron net-create external_network --provider:network_type flat \
-    --provider:physical_network extnet  \
-    --router:external \
-    --shared
+openstack flavor create --id 0 --vcpus 1 --ram 64 --disk 1 m1.nano
 
-echo "Tao subnet cho external network"
-sleep 3
-neutron subnet-create --name public_subnet \
-    --enable_dhcp=True --dns-nameserver 8.8.8.8 \
-    --allocation-pool=start=172.16.69.80,end=172.16.69.100 \
-    --gateway=172.16.69.1 external_network 172.16.69.0/24
-
-echo "Tao private network"
-sleep 3
-neutron net-create private_network
-    neutron subnet-create --name private_subnet private_network 10.0.0.0/24 \
-    --dns-nameserver 8.8.8.8
-
-echo "Tao router va gan cac interface cho router"
-sleep 3
-neutron router-create router
-    neutron router-gateway-set router external_network
-    neutron router-interface-add router private_subnet
-
-echo "Tao cac rule trong security group"
-sleep 3
+echocolor "Mo rule ping"
+sleep 5
 openstack security group rule create --proto icmp default
 openstack security group rule create --proto tcp --dst-port 22 default
 
-echo "Ket thuc"
+echocolor "Tao provider network"
+sleep 3
+openstack network create  --share --external \
+  --provider-physical-network provider \
+  --provider-network-type flat extnet
+
+echocolor "Tao subnet cho provider network"
+sleep 3
+openstack subnet create --network extnet \
+	--allocation-pool start=$PROVIDER_IP_START,end=$PROVIDER_IP_END \
+	--dns-nameserver $PROVIDER_DNS --gateway $PROVIDER_GATEWAY \
+	--subnet-range $PROVIDER_SUBNET extnet
+  
+echocolor "Tao VM gan vao provider network"
+sleep 5
+
+PROVIDER_NET_ID=`openstack network list | egrep -w extnet | awk '{print $2}'`
+
+ID_ADMIN_PROJECT=`openstack project list | grep admin | awk '{print $2}'`
+ID_SECURITY_GROUP=`openstack security group list | grep $ID_ADMIN_PROJECT | awk '{print $2}'`
+
+openstack server create --flavor m1.nano --image cirros \
+	--nic net-id=$PROVIDER_NET_ID --security-group $ID_SECURITY_GROUP \
+	provider-VM1
+
+###############################################################################
+echocolor "Tao private network (selfservice network)"
+sleep 3
+openstack network create selfservice
+
+echocolor "Tao subnnet cho private network"
+sleep 3
+ openstack subnet create --network selfservice \
+ 	--dns-nameserver $PRIVATE_DNS --gateway $PRIVATE_GATEWAY \
+ 	--subnet-range $PRIVATE_SUBNET selfservice
+
+echocolor "Tao va gan inteface cho ROUTER"
+sleep 3
+openstack router create R1
+neutron router-interface-add R1 selfservice
+neutron router-gateway-set R1 extnet
+
+echocolor "Tao may ao gan vao private network (selfservice network)"
+sleep 5
+ID_ADMIN_PROJECT=`openstack project list | grep admin | awk '{print $2}'`
+ID_SECURITY_GROUP=`openstack security group list | grep $ID_ADMIN_PROJECT | awk '{print $2}'`
+
+PRIVATE_NET_ID=`openstack network list | egrep -w selfservice | awk '{print $2}'`
+
+openstack server create --flavor m1.nano --image cirros \
+  --nic net-id=$PRIVATE_NET_ID --security-group $ID_SECURITY_GROUP \
+  selfservice-VM1
+
+
+echocolor "Floatig IP"
+sleep 5
+FLOATING_IP=`openstack floating ip create extnet | egrep -w floating_ip_address | awk '{print $4}'`
+openstack server add floating ip selfservice-VM1 $FLOATING_IP
